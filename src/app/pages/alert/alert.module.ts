058e949e465e1c271e542b67cdad20cddc208253
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2023-01-27 15:22:55 -0300

    Aula - 13 - Módulos e Componentes

--- src/app/pages/alert/alert.module.ts
+++ src/app/pages/alert/alert.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { AlertPageRoutingModule } from './alert-routing.module';
 
 import { AlertPage } from './alert.page';
+import { ComponentesModule } from 'src/app/componentes/componentes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AlertPageRoutingModule
+    AlertPageRoutingModule,
+    ComponentesModule
   ],
   declarations: [AlertPage]
 })
