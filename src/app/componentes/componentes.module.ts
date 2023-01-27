65709eeb45cd3e15ecdf264b515099ddf477ef18
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2023-01-27 15:32:54 -0300

    Aula - 14 - Chamando Componentes

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -1,12 +1,18 @@
+import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
+  exports: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class ComponentesModule { }
